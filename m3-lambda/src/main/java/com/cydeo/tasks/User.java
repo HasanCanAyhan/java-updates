82f2e0e4c655d26775d9af4bca419a9912684018
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-16 21:34:00 +0200

    lamda

---
+++ m3-lambda/src/main/java/com/cydeo/tasks/User.java
@@ -0,0 +1,15 @@
+package com.cydeo.tasks;
+
+import lombok.*;
+
+@Data // Getter Setter ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+}
---
+++ m3-lambda/src/main/java/com/cydeo/tasks/UserTest.java
@@ -0,0 +1,20 @@
+package com.cydeo.tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserTest {
+
+    public static void main(String[] args) {
+
+        List<User> users = new ArrayList<>();
+
+        users.add(new User("Mike","Smith",35));
+        users.add(new User("Tom","Evan",25));
+        users.add(new User("Emma","Pollard",55));
+
+
+
+    }
+
+}
