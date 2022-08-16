1b0fe6614aab1fc18b60ed679c435e2b669b17a3
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-16 18:24:30 +0200

    lambda

---
+++ m3-lambda/src/main/java/com/cydeo/OrangeFormatter.java
@@ -0,0 +1,6 @@
+package com.cydeo;
+
+@FunctionalInterface
+public interface OrangeFormatter { // different action
+    String accept(Orange orange); // one orange object
+}
---
+++ m3-lambda/src/main/java/com/cydeo/OrangeTest.java
@@ -0,0 +1,23 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrangeTest {
+
+    public static void main(String[] args) {
+
+        List<Orange> inventory = new ArrayList<Orange>();
+
+        inventory.add(new Orange(300,Color.GREEN));
+        inventory.add(new Orange(100,Color.RED));
+        inventory.add(new Orange(200,Color.GREEN));
+        inventory.add(new Orange(50,Color.RED));
+
+
+
+
+    }
+
+
+}
