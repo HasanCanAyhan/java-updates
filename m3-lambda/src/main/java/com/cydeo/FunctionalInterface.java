754c72f122d92b2eb932ad20161e6adffe3595d7
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-16 20:28:27 +0200

    lambda

---
+++ m3-lambda/src/main/java/com/cydeo/FunctionalInterface.java
@@ -0,0 +1,31 @@
+package com.cydeo;
+
+public class FunctionalInterface {
+
+    public static void main(String[] args) {
+
+        /*
+        //Anonismus class
+
+        ApplePredicate ap = new ApplePredicate() { // AppleHeavyPredicate class
+            @Override
+            public boolean test(Apple apple) {
+                return apple.getWeight() > 200;
+            }
+        };
+
+
+         */
+
+        // we will choose interface according to Signature (parameter and return type)
+
+        //************PREDICATE************
+
+
+
+
+
+
+    }
+
+}
