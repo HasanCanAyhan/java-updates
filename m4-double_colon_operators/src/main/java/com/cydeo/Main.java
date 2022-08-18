a095058849c9c0c2704f8d921832374001f216df
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-18 17:26:27 +0200

    double colon operators

--- m4-double_colon_operators/src/main/java/com/cydeo/Main.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -1,8 +1,12 @@
 package com.cydeo;
 
+import javax.sql.rowset.Predicate;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Predicate<Integer> pred = (p) -> p % 2 == 0 ? true : false; // the implementation of abstract method inside the Functional Interface
 
 
 
