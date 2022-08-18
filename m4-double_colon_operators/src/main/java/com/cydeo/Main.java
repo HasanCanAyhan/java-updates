528793b9637bfbaf0f8be580663535fe12e5b607
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-18 17:49:28 +0200

    double colon operators

--- m4-double_colon_operators/src/main/java/com/cydeo/Main.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -14,9 +14,16 @@ public class Main {
 
         Calculate s1 = (x,y) -> Calculator.findSum(x,y); // implementation from the ready method which written before.
 
+        //Reference to a Static Method
         Calculate s2 = Calculator ::findSum;
-
         s2.calculate(10,20);
+
+        //Reference to an instance method
+        Calculator obj = new Calculator();
+        Calculate s3 = obj::findMultiply;
+
+        //or : you can write this : same
+        Calculate s4 = new Calculator()::findMultiply;
 
 
 
