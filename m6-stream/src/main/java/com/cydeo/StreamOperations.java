212c755856ee4478284ac8ba7672452df5e11323
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-19 18:02:12 +0200

    stream

--- m6-stream/src/main/java/com/cydeo/StreamOperations.java
+++ m6-stream/src/main/java/com/cydeo/StreamOperations.java
@@ -8,7 +8,7 @@ public class StreamOperations {
 
     public static void main(String[] args) {
 
-        List<Integer> list = Arrays.asList(1,2,2,3,4);
+        List<Integer> list = Arrays.asList(1,2,3,4);
         //list.forEach( x -> System.out.println(x));
         list.forEach(System.out::println);
 
@@ -34,7 +34,12 @@ public class StreamOperations {
                 .limit(1)
                 .forEach(System.out::println);
 
+        System.out.println("Skip: ");
 
+        list.stream()
+                .filter( i -> i %2 ==0 )
+                .skip(1)
+                .forEach(System.out::println);
 
     }
 
